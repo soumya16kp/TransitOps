a43222fe0403b4db9fbc09f7bf7c8c8afa38f1e8
kapil shivhare <kapilshivhare9111728981@gmail.com>
2026-07-12 12:46:05 +0530

    Fix syntax error in urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.contrib import admin
 from django.urls import path, include
 
@@ -12,15 +11,3 @@ urlpatterns = [
     path('api/maintenance/', include('maintenance_app.urls')),
     path('api/expenses/', include('expenses.urls')),
 ]
-=======
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/auth/', include('auth_app.urls')),
-    path('api/core/', include('core.urls')),
-    path('api/vehicles/', include('vehicle_app.urls')),
-    path('api/drivers/', include('driver_app.urls')),
-]
->>>>>>> 314f758dc6c55a198e2463701587c02327a369fe
